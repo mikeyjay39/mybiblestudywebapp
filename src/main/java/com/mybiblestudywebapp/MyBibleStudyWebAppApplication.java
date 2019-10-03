5cfd9857eaf345a962c166a652bacfcba60011fe
Michael Jeszenka <michael@jeszenka.com>
2019-10-04 00:52:51 +0200

    Init repo
    First commit

---
+++ src/main/java/com/mybiblestudywebapp/MyBibleStudyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.mybiblestudywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBibleStudyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBibleStudyWebAppApplication.class, args);
+	}
+
+}
