8b01430a70eda040a0fad4c893aabdd78c43d515
Michael Jeszenka <michael@jeszenka.com>
2019-10-15 19:53:57 +0200

    Refactor schema
    Use bigints for primary keys. Found out how to properly export and
    import schema without any erros. Instructions for this added to
    README.

---
+++ README.md
@@ -0,0 +1,6 @@
+__Backup Schema__
+
+1. Right click on "public" in datagrip
+2. SQL Scripts > SQL Generator
+3. Checkmark "Use create or replace syntax" AND "Ignore owner"
+4. Save to schema.sql
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,120 +1,106 @@
-comment on schema public is 'standard public schema';
-
-alter schema public owner to postgres;
-
 create table "user"
 (
-    user_id serial not null
+    user_id    bigserial    not null
         constraint user_pk
             primary key,
-    email varchar(128) not null,
-    firstname varchar(64),
-    lastname varchar(64),
-    password varchar(128) not null,
-    ranking integer default 0,
+    email      varchar(128) not null,
+    firstname  varchar(64),
+    lastname   varchar(64),
+    password   varchar(128) not null,
+    ranking    integer   default 0,
     created_at timestamp default now()
 );
-
-
 
 create unique index user_email_uindex
     on "user" (email);
 
 create table view
 (
-    view_id serial not null
+    view_id   bigserial   not null
         constraint view_pk
             primary key,
-    user_id integer not null
+    user_id   bigint      not null
         constraint view_user__fk
             references "user",
     view_code varchar(64) not null,
-    private boolean default false
+    priv      boolean default false
 );
-
-
 
 create unique index view_view_code_uindex
     on view (view_code);
 
 create table book
 (
-    book_id serial not null
+    book_id   bigserial   not null
         constraint book_pk
             primary key,
-    title varchar(64) not null,
+    title     varchar(64) not null,
     testament varchar(2)
 );
-
-
 
 create unique index book_title_uindex
     on book (title);
 
 create table chapter
 (
-    chapter_id serial not null
+    chapter_id bigserial not null
         constraint chapter_pk
             primary key,
-    book_id integer not null
+    book_id    bigint    not null
         constraint chapter_book__fk
             references book,
-    chapter_no integer not null
+    chapter_no integer   not null
 );
-
 
 create table note
 (
-    note_id serial not null
+    note_id       bigserial               not null
         constraint note_pk
             primary key,
-    note text,
-    user_id integer
+    note          text,
+    user_id       bigint
         constraint note_user__fk
             references "user",
-    book_id integer
+    book_id       bigint
         constraint note_book__fk
             references book,
-    chapter_id integer
+    chapter_id    bigint
         constraint note_chapter__fk
             references chapter,
-    verse integer not null,
-    ranking integer default 0,
-    private boolean default false not null,
-    lang varchar(64),
-    created_at timestamp default now() not null,
+    verse         integer                 not null,
+    ranking       integer   default 0,
+    priv          boolean   default false not null,
+    lang          varchar(64),
+    created_at    timestamp default now() not null,
     last_modified timestamp
 );
 
-
 create table view_note
 (
-    view_note_id integer not null
+    view_note_id bigserial not null
         constraint view_note_pk
             primary key,
-    view_id integer
+    view_id      bigint
         constraint view_note_view__fk
             references view,
-    note_id integer
+    note_id      bigint
         constraint view_note_note__fk
             references note
 );
 
-
 create table comment
 (
-    comment_id integer not null
+    comment_id bigint not null
         constraint comment_pk
             primary key,
-    user_id integer
+    user_id    integer
         constraint comment_user_user_id_fk
             references "user",
-    note_id integer
+    note_id    integer
         constraint comment_note_note_id_fk
             references note,
     created_at timestamp default now()
 );
 
 comment on table comment is 'Used for user comments on notes';
-
 
