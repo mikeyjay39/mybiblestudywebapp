7b0bc0c0ec3f24ae955bca0cc85a76d7653cf23c
Michael Jeszenka <mikeyjay39@gmail.com>
2019-10-17 13:57:31 -0700

    Add all rows to chapters table

--- src/main/java/com/mybiblestudywebapp/persistence/ChapterDao.java
+++ src/main/java/com/mybiblestudywebapp/persistence/ChapterDao.java
@@ -42,10 +42,79 @@ public class ChapterDao implements Dao<Chapter> {
 
     /**
      * Use this method to insert all the chapters needed into the data base.
-     * @param args Key = book_id. Value = number of chapters in the book.
+     *
      * @return
      */
-    public long insertAllChapters(TreeMap<Integer, Integer> args) {
+    private long insertAllChapters() {
+        TreeMap<Integer, Integer> args = new TreeMap<>();
+        args.put(1, 50);
+        args.put(2, 40);
+        args.put(3, 27);
+        args.put(4, 36);
+        args.put(5, 34);
+        args.put(6, 24);
+        args.put(7, 21);
+        args.put(8, 4);
+        args.put(9, 31);
+        args.put(10, 24); // 2 Samuel
+        args.put(11, 22);
+        args.put(12, 25);
+        args.put(13, 29);
+        args.put(14, 36);
+        args.put(15, 10);
+        args.put(16, 13);
+        args.put(17, 10);
+        args.put(18, 42);
+        args.put(19, 150);
+        args.put(20, 31); // Proverbs
+        args.put(21, 12);
+        args.put(22, 8);
+        args.put(23, 66);
+        args.put(24, 52);
+        args.put(25, 5);
+        args.put(26, 48);
+        args.put(27, 12);
+        args.put(28, 14);
+        args.put(29, 3);
+        args.put(30, 9);
+        args.put(31, 1);
+        args.put(32, 4);
+        args.put(33, 7);
+        args.put(34, 3);
+        args.put(35, 3);
+        args.put(36, 3);
+        args.put(37, 2);
+        args.put(38, 14);
+        args.put(39, 4);
+        args.put(40, 28); // Matthew
+        args.put(41, 16);
+        args.put(42, 24);
+        args.put(43, 21);
+        args.put(44, 28);
+        args.put(45, 16);
+        args.put(46, 16);
+        args.put(47, 13);
+        args.put(48, 6);
+        args.put(49, 6);
+        args.put(50, 4); // Philippians
+        args.put(51, 4);
+        args.put(52, 5);
+        args.put(53, 3);
+        args.put(54, 6);
+        args.put(55, 4);
+        args.put(56, 3);
+        args.put(57, 1);
+        args.put(58, 13);
+        args.put(59, 5);
+        args.put(60, 5);
+        args.put(61, 3);
+        args.put(62, 5);
+        args.put(63, 1);
+        args.put(64, 1);
+        args.put(65, 1);
+        args.put(66, 22);
+
+
         long totalRows = 0;
         for (int i = 0; i < args.size(); i++) {
             int book_id = i + 1;
--- src/test/java/com/mybiblestudywebapp/unittests/ChapterUnitTests.java
+++ src/test/java/com/mybiblestudywebapp/unittests/ChapterUnitTests.java
@@ -6,8 +6,10 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.TreeMap;
 
 /**
@@ -38,7 +40,7 @@ public class ChapterUnitTests {
 
     @Test
     public void testGetById() throws Exception {
-        long rows = insertChapters();
+        //long rows = chapterDao.insertAllChapters();
 
         // get first chapter in Genesis
         chapter = chapterDao.get(1).get();
@@ -49,76 +51,36 @@ public class ChapterUnitTests {
         Assert.assertEquals(2, chapter.getBookId());
     }
 
-    private long insertChapters() {
-        TreeMap<Integer, Integer> args = new TreeMap<>();
-        args.put(1, 50);
-        args.put(2, 40);
-        args.put(3, 27);
-        args.put(4, 36);
-        args.put(5, 34);
-        args.put(6, 24);
-        args.put(7, 21);
-        args.put(8, 4);
-        args.put(9, 31);
-        args.put(10, 24); // 2 Samuel
-        args.put(11, 22);
-        args.put(12, 25);
-        args.put(13, 29);
-        args.put(14, 36);
-        args.put(15, 10);
-        args.put(16, 13);
-        args.put(17, 10);
-        args.put(18, 42);
-        args.put(19, 150);
-        args.put(20, 31); // Proverbs
-        args.put(21, 12);
-        args.put(22, 8);
-        args.put(23, 66);
-        args.put(24, 52);
-        args.put(25, 5);
-        args.put(26, 48);
-        args.put(27, 12);
-        args.put(28, 14);
-        args.put(29, 3);
-        args.put(30, 9);
-        args.put(31, 1);
-        args.put(32, 4);
-        args.put(33, 7);
-        args.put(34, 3);
-        args.put(35, 3);
-        args.put(36, 3);
-        args.put(37, 2);
-        args.put(38, 14);
-        args.put(39, 4);
-        args.put(40, 28); // Matthew
-        args.put(41, 16);
-        args.put(42, 24);
-        args.put(43, 21);
-        args.put(44, 28);
-        args.put(45, 16);
-        args.put(46, 16);
-        args.put(47, 13);
-        args.put(48, 6);
-        args.put(49, 6);
-        args.put(50, 4); // Philippians
-        args.put(51, 4);
-        args.put(52, 5);
-        args.put(53, 3);
-        args.put(54, 6);
-        args.put(55, 4);
-        args.put(56, 3);
-        args.put(57, 1);
-        args.put(58, 13);
-        args.put(59, 5);
-        args.put(60, 5);
-        args.put(61, 3);
-        args.put(62, 5);
-        args.put(63, 1);
-        args.put(64, 1);
-        args.put(65, 1);
-        args.put(66, 22);
+    /**
+     * Use this once on the live DB to build rows for chapters table
+     * @throws Exception
+     */
+    /*@Test
+    public void insertAllChapters() throws Exception {
 
-        long updatedRows = chapterDao.insertAllChapters(args);
-        return updatedRows;
-    }
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(System.getenv("PSQLDBDRIVER"));
+        dataSourceBuilder.url(System.getenv("PSQLDBURL"));
+        dataSourceBuilder.username(System.getenv("PSQLDBUSER"));
+        dataSourceBuilder.password(System.getenv("PSQLDBPASS"));
+        DataSource dataSource = dataSourceBuilder.build();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        chapterDao = new ChapterDao(jdbcTemplate);
+
+
+        long rows = chapterDao.insertAllChapters();
+
+        Assert.assertEquals(1189, rows);
+
+        // get first chapter in Genesis
+        chapter = chapterDao.get(1).get();
+        Assert.assertEquals(1, chapter.getChapter_no());
+        // get first chapter in Exodus
+        chapter = chapterDao.get(51).get();
+        Assert.assertEquals(1, chapter.getChapter_no());
+        Assert.assertEquals(2, chapter.getBookId());
+    }*/
+
+
 }
