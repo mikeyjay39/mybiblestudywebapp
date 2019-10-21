dd40be310f2617999804a0d0ae95e23c7ce82ba5
Michael Jeszenka <mikeyjay39@gmail.com>
2019-10-21 14:27:18 -0700

    Add GetBibleService

---
+++ src/main/java/com/mybiblestudywebapp/bible/GetBibleService.java
@@ -0,0 +1,23 @@
+package com.mybiblestudywebapp.bible;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by Michael Jeszenka.
+ * <a href="mailto:michael@jeszenka.com">michael@jeszenka.com</a>
+ * 10/21/19
+ */
+public interface GetBibleService {
+
+    /**
+     * Get verses for a chapter
+     *
+     * @param book
+     * @param chapter
+     * @return
+     */
+    CompletableFuture<List<Map<String, String>>> getVersesForChapter(String book, int chapter);
+
+}
---
+++ src/test/java/com/mybiblestudywebapp/integrationtests/GetBibleServiceTest.java
@@ -0,0 +1,31 @@
+package com.mybiblestudywebapp.integrationtests;
+
+import com.mybiblestudywebapp.bible.GetBibleService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael Jeszenka.
+ * <a href="mailto:michael@jeszenka.com">michael@jeszenka.com</a>
+ * 10/21/19
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GetBibleServiceTest {
+
+    @Autowired
+    private GetBibleService getBibleService;
+
+    @Test
+    public void getVersesForChapter() throws Exception {
+        var result = getBibleService.getVersesForChapter("Genesis",  1).get();
+        Assert.assertNotNull(result);
+    }
+}
