3cb04cb2e346e50cc570430ecddf7ef7b99fc85e
Michael Jeszenka <mikeyjay39@gmail.com>
2019-10-27 19:17:10 -0700

    Add UserControllerTest.java
    TODO- create this test

---
+++ src/test/java/com/mybiblestudywebapp/integrationtests/UsersControllerTest.java
@@ -0,0 +1,19 @@
+package com.mybiblestudywebapp.integrationtests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael Jeszenka.
+ * <a href="mailto:michael@jeszenka.com">michael@jeszenka.com</a>
+ * 10/27/19
+ */
+public class UsersControllerTest {
+
+    // TODO - create test for this
+
+    @Test
+    public void create() {
+    }
+}
