b8f5b1dc94af8c63b23e5f618c56872adbceae81
Michael Jeszenka <mikeyjay39@gmail.com>
2019-11-11 15:29:55 -0800

    Add XSS protection

---
+++ src/main/java/com/mybiblestudywebapp/security/xss/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes to fight against XSS
+ * Created by Michael Jeszenka.
+ * <a href="mailto:michael@jeszenka.com">michael@jeszenka.com</a>
+ * 11/11/19
+ */
+package com.mybiblestudywebapp.security.xss;
