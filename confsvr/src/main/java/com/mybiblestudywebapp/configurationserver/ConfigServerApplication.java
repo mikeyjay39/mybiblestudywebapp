9ea9efb746359bdf50ad1f5cdb80ee2f7fb90244
Michael Jeszenka <mikeyjay39@gmail.com>
2020-03-13 14:00:39 -0700

    Fix cloud config dependency problem

--- confsvr/src/main/java/com/mybiblestudywebapp/configurationserver/ConfigServerApplication.java
+++ confsvr/src/main/java/com/mybiblestudywebapp/configurationserver/ConfigServerApplication.java
@@ -2,12 +2,16 @@ package com.mybiblestudywebapp.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        run(ConfigServerApplication.class, args);
     }
 
 }
