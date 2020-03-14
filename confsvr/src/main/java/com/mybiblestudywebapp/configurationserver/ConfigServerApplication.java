7189a5f95a2f469985c13fbc57b01cbc613986e6
Michael Jeszenka <mikeyjay39@gmail.com>
2020-03-13 18:56:42 -0700

    Fix intellij mvn dependency problems

--- confsvr/src/main/java/com/mybiblestudywebapp/configurationserver/ConfigServerApplication.java
+++ confsvr/src/main/java/com/mybiblestudywebapp/configurationserver/ConfigServerApplication.java
@@ -2,16 +2,12 @@ package com.mybiblestudywebapp.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-
-import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
