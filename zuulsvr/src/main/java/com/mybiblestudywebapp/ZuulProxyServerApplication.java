9d62d296ed867fd343eb55210e85eadb393910b6
Michael Jeszenka <mikeyjay39@gmail.com>
2020-03-19 12:28:32 -0700

    Add Zuul

---
+++ zuulsvr/src/main/java/com/mybiblestudywebapp/ZuulProxyServerApplication.java
@@ -0,0 +1,13 @@
+package com.mybiblestudywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyServerApplication.class, args);
+    }
+
+}
---
+++ zuulsvr/src/test/java/com/mybiblestudywebapp/ZuulProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybiblestudywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
