48c619d97b09f2516c3e85e2570a0585514ac737
Michael Jeszenka <mikeyjay39@gmail.com>
2020-03-21 19:57:31 -0700

    Add authservice issues token on good password

--- authenticationservice/src/main/java/com/mybiblestudywebapp/authenticationservice/security/OAuth2Config.java
+++ authenticationservice/src/main/java/com/mybiblestudywebapp/authenticationservice/security/OAuth2Config.java
@@ -1,12 +1,34 @@
 package com.mybiblestudywebapp.authenticationservice.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Michael Jeszenka.
@@ -17,14 +39,64 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private DataSource dataSource;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean(name = "jcds")
+    public JdbcClientDetailsService clientDetailsService() {
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+
+    @Bean
+    public AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+   /* @Bean
+    public ClientDetailsService clientDetailsServiceBean() {
+        return new ClientDetailsService() {
+            @Override
+            public ClientDetails loadClientByClientId(String s) throws ClientRegistrationException {
+                BaseClientDetails details = new BaseClientDetails();
+                details.setClientId(s);
+                details.setAuthorizedGrantTypes(Arrays.asList("refresh_token", "password", "client_credentials") );
+                details.setScope(Arrays.asList("webclient", "mobileclient"));
+                //details.setRegisteredRedirectUri(Collections.singleton("http://anywhere.com"));
+                //details.setResourceIds(Arrays.asList("oauth2-resource"));
+                Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                details.setAuthorities(authorities);
+                return details;
+            }
+        };
+    }*/
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
+        clients
+                /*withClientDetails(clientDetailsServiceBean()
+                )
+                .jdbc().withClient("mybiblestudywebapp")
+                .secret("secret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("webclient", "mobileclient");
+                //.and().build();*/
+                .inMemory()
                 .withClient("mybiblestudywebapp")
                 .secret("secret")
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials")
@@ -32,9 +104,24 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     }
 
     @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+
+        oauthServer.allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager).tokenStore(tokenStore());
+                /*.approvalStore(approvalStore())
+                .authorizationCodeServices(authorizationCodeServices())
+                .tokenStore(tokenStore());*/
+    }
+
+    /*@Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
-    }
+    }*/
 }
--- authenticationservice/src/main/java/com/mybiblestudywebapp/authenticationservice/security/WebSecurityConfigurer.java
+++ authenticationservice/src/main/java/com/mybiblestudywebapp/authenticationservice/security/WebSecurityConfigurer.java
@@ -1,15 +1,23 @@
 package com.mybiblestudywebapp.authenticationservice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.security.SecureRandom;
 
 /**
@@ -18,6 +26,7 @@ import java.security.SecureRandom;
  * 3/21/20
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -25,17 +34,31 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
+    /*@Bean//(name = "webUserDetails")
+    @Override
     public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+        return userDetailsManager;
+    }*/
 
     @Bean
     public PasswordEncoder encoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/token")
+                .permitAll().
+                anyRequest().authenticated();
+    }
 
+/*
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
@@ -46,5 +69,32 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         .and()
         .passwordEncoder(encoder());
 
+    }*/
+
+    @Autowired
+    private DataSource dataSource;
+
+    /*private static final String SALT = "thisisasalt";
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }*/
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT email, password, enabled FROM users " +
+                                "WHERE email = ?"
+                )
+                .authoritiesByUsernameQuery(
+                        "SELECT email, authority FROM user_authorities " +
+                                "WHERE email = ?"
+                )
+                .passwordEncoder(encoder())
+        ;
     }
 }
