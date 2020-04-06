150b3355487fd8122f5fb8fdaaa5669aca0e7b4b
Michael Jeszenka <mikeyjay39@gmail.com>
2020-04-06 15:52:18 -0700

    Add run.sh to authserver dockerfile
    Need to add a ping and wait against the config server to make sure
    auth server isn't started before the config server is ready.

---
+++ authenticationservice/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#echo "********************************************************"
+#echo "Waiting for the eureka server to start on port $EUREKASERVER_URI"
+#echo "********************************************************"
+#while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
+#echo "******* Eureka Server has started"
+#
+#
+#echo "********************************************************"
+#echo "Waiting for the database server to start on $PSQLDBURL"
+#echo "********************************************************"
+#while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+#echo "******** Database Server has started "
+#
+#echo "********************************************************"
+#echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
+#echo "********************************************************"
+#while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
+#echo "*******  Configuration Server has started"
+#
+#
+#echo "********************************************************"
+#echo "Waiting for the kafka server to start on port $KAFKASERVER_PORT"
+#echo "********************************************************"
+#while ! `nc -z kafkaserver $KAFKASERVER_PORT`; do sleep 10; done
+#echo "******* Kafka Server has started"
+#
+#echo "********************************************************"
+#echo "Waiting for the REDIS server to start  on port $REDIS_PORT"
+#echo "********************************************************"
+#while ! `nc -z redis $REDIS_PORT`; do sleep 10; done
+#echo "******* REDIS has started"
+#
+#echo "********************************************************"
+#echo "Starting License Server with Configuration Service via Eureka :  $EUREKASERVER_URI:$SERVER_PORT"
+#echo "Using Kafka Server: $KAFKASERVER_URI"
+#echo "Using ZK    Server: $ZKSERVER_URI"
+echo "********************TESTING************************************"
+java -jar app.jar
