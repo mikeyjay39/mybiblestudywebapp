7df37b8082ebc342a0d77a60e07ed1dd11569026
Michael Jeszenka <mikeyjay39@gmail.com>
2020-04-08 18:25:10 -0700

    Fix bibletextservice for docker and non docker
    Should now run in both environments. Took out the building of the
    bibltext db tables upon start up and mounted it as a volume on the
    local file system.

--- mybiblestudyservice/dbrun.sh
+++ mybiblestudyservice/dbrun.sh
@@ -4,7 +4,7 @@
 docker run --rm --name redis-docker -d -p 6379:6379 redis:alpine
 #docker run --rm --name kafka-docker -d -p 2181:2181 -p 9092:9092 spotify/kafka
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=$(echo $PSQLPASSWORD) -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
-docker run --rm --name pg-docker-bibletext -e POSTGRES_PASSWORD=$(echo $PSQLPASSWORD) -d -p 5434:5432 postgres
+docker run --rm --name pg-docker-bibletext -e POSTGRES_PASSWORD=$(echo $PSQLPASSWORD) -d -p 5434:5432 -v ${HOME}/docker/volumes/postgres-bibles:/var/lib/postgresql/data postgres
 #docker run --rm  --name kafka-docker -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --env TOPICS=persistenceServiceTopic spotify/kafka
 
 
